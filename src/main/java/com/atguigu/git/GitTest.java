ebe822f248479af0ffda74df06ef5cbd8535809c
yangzh <yangzh@cncbox.com>
2021-12-21 15:14:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Author YZH
+ * @Date 2021/12/21 15:10
+ * @Description
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
