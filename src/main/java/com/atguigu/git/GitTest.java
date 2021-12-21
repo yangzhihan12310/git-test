2bb1d67b0167c69c0d1166ff68b3d97ec11d0079
yangzh <yangzh@cncbox.com>
2021-12-21 15:16:22 +0800

    first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,8 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
