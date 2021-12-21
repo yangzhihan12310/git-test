150b9d66bc01e99dafa5316a7be6552cdd065745
yangzh <yangzh@cncbox.com>
2021-12-21 15:36:39 +0800

    3 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,8 +11,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git2");
-        System.out.println("hello git2");
-        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
